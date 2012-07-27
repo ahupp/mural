2cbbd132216f7406f9e1170ad8c25ae8d8c0cfe0
Adam Hupp <ahupp@fb.com>
2012-07-27 11:38:24 -0700

    [mural] Multiple repo support and doc updates
    Summary:
    Adds support for multiple repositories/tag files.  Tag files are now
    specified by
    
      (mural-add-tagfile "~/www/TAGS")
      (mural-add-tagfile "~/local/hphp/src/TAGS")
    
    The tagfile/repo to choose is based on the location of the currently
    open file (e.g files in hphp/src search that tags file, files in ~/www
    use that one).
    
    Test Plan:
    C-o in an directory without a tags file returns error
    
    Opening a file in hphp/src or www/ use the associated tags file.
    
    Killing the muralserver buffer/proces does not lead to a leak in the
    tagfile-to-process alist.

--- mural.el
+++ mural.el
@@ -1,15 +1,37 @@
 ;;
-;; mural does fast, fuzzy tag searching that refines results as you
-;; type.
+;; mural adds a fast, fuzzy tag search typeahead to emacs.
+;;
+;; Features:
+;;
+;;   -- fuzzy search means that you can type any sequence of letters
+;;      in a tag.  e.g, 'prof_get_s' will find 'profile_get_short'.
+;;      This lets you find tags who's name you only vagely remember,
+;;      is robust to some typos, and only requires typing a fraction
+;;      of the characters in most case (e.g
+;;      SimpleBeanFactoryAwareAspectInstanceFactory could by found by
+;;      just typing SBFAAIF, or some subset thereof)
+;;
+;;   -- results are refined on each keypress, which lets you stop as
+;;      soon as an unambiguous result is found.
+;;
+;;   -- it's dramatically faster than pure emacs solutions for large
+;;      tags files.  Single queries return in 35ms, compared to
+;;      several seconds to do a tab completion in find-tag and over
+;;      10s for a fuzzy match in ido-mode.
+;;
+;;   -- supports multiple tag files and repositories.  The repo to
+;;      search is chosen based on the location of the file in the
+;;      current buffer.
+;;
+;;   -- changes to the tags file are picked up without a restart using inotify.
+;;
 ;;
 ;; To use, put this in your .emacs:
 ;;
 ;;   (require 'mural)
-;;   ;; not strictly necessary, but avoids slowness on first use
-;;   (mural-get-server)
-;;   (global-set-key (kbd "C-o") 'mural-find-tag)
-;;
-;; If it is not already set make sure tags-file-name points at your tags file.
+;;   (mural-add-tagfile "~/www/TAGS")
+;;   (mural-add-tagfile "~/local/hphp/src/TAGS")
+;;   (global-set-key (kbd "C-o") 'mural-open-dwim)
 ;;
 ;; To debug launch:
 ;; (call-process mural-server-path nil t nil tags-file-name)
@@ -21,24 +43,42 @@
 
 (defvar mural-server-echo nil
   "echo server responses to *muralserver* buffer")
-(defvar mural-server-process nil)
-(defvar mural-server-output "")
+
 (defvar ido-dynamic-match-fn nil)
+(defvar mural-tagfile-to-process '())
+
+;; Only used in the context of a single query
+(defvar mural-server-output "")
 (defvar mural-last-query-result nil)
+(defvar mural-current-tagfile nil)
 
-(defun mural-get-server ()
-  (if (or (not mural-server-process)
-          (not (eq (process-status mural-server-process) 'run)))
-      (setq mural-server-process (mural-start-server)))
-  mural-server-process)
+(defun mural-add-tagfile (tagfile)
+  (mural-get-server (expand-file-name tagfile)))
 
-(defun mural-start-server ()
-  (if (not tags-file-name)
-      (error "tags-file-name not set"))
+(defun mural-add-tag-process-entry (tagfile process)
+  (setq mural-tagfile-to-process
+        (cons
+         (cons tagfile process)
+         (filter (lambda (entry)
+                   (not (equal tagfile (car entry))))
+                 mural-tagfile-to-process))))
+
+(defun mural-get-server (tagfile)
+  (let ((proc (cdr (assoc tagfile mural-tagfile-to-process))))
+    (if (or (not proc)
+            (not (eq (process-status proc) 'run)))
+        (let ((newproc (mural-start-server tagfile)))
+          (mural-add-tag-process-entry tagfile newproc)
+          newproc)
+      proc)))
+
+(defun mural-start-server (tagfile)
+  (if (not tagfile)
+      (error "tagfile not provided"))
   (message "Starting mural server")
   (let ((proc (start-process
                "mural" "*muralserver*"
-               mural-server-path tags-file-name)))
+               mural-server-path tagfile)))
     (set-process-filter
      proc
      (lambda (process output)
@@ -52,18 +92,38 @@
            (setq mural-server-output (concat mural-server-output output))))))
      proc))
 
-(defun mural-query (query)
+;; I can't believe I have to write this function
+(defun filter (pred lst)
+  (if (eq 0 (length lst))
+      '()
+    (let ((fst (car lst))
+          (rst (filter pred (cdr lst))))
+      (if (funcall pred fst)
+        (cons fst rst)
+        rst))))
+
+(defun mural-tagfile-for-filename (filename)
+  (let ((resolved-fname (file-truename filename)))
+    (car (filter
+          (lambda (tagfile)
+            ;; file-truename resolves symlinks so we make sure to compare the same thing
+            (eq 0 (string-match
+                   (file-truename (file-name-directory tagfile))
+                   resolved-fname)))
+          (mapcar 'car mural-tagfile-to-process)))))
+
+(defun mural-query (query tagfile)
   (if (equal "" query)
       '()
-    (progn
+    (let ((proc (mural-get-server tagfile)))
       (setq mural-server-output "")
-      (process-send-string (process-name (mural-get-server))
+      (process-send-string (process-name proc)
                            (concat query "\n"))
 
       ;; Wait up to 1/2 second
       (loop for i from 1 to 10
             until (mural-response-complete mural-server-output)
-            do (accept-process-output mural-server-process .05))
+            do (accept-process-output proc .05))
       (if (mural-response-complete mural-server-output)
           (mural-parse-response mural-server-output)
         (progn
@@ -72,9 +132,9 @@
         ))))
 
 (defun mural-query-save-tag (query)
-  (let ((qresult (mural-query query)))
+  (let ((qresult (mural-query query mural-current-tagfile)))
     (setq mural-last-query-result qresult)
-    (mapcar (lambda (x) (elt x 0)) qresult)))
+    (mapcar 'mural-tag-tagname qresult)))
 
 ;;(mural-parse-response "MATCH foo bar 1\nMATCH hi bye 2\nDONE hi hi")
 ;; returns '((tag file row) ...)
@@ -95,11 +155,19 @@
   (integerp (string-match "^DONE" result)))
 
 (defun mural-read-tag ()
-  (setq ido-dynamic-match-fn 'mural-query-save-tag)
-  (unwind-protect ;; i have no idea what's going on here
-      (ido-completing-read "tag: " '("nope"))
-    (setq ido-dynamic-match-fn nil)))
+  (let ((file-base (buffer-file-name)))
+    (if (not file-base)
+        (error "no file associated with buffer"))
+    (setq mural-current-tagfile (mural-tagfile-for-filename file-base))
+    (if (not mural-current-tagfile)
+        (error "no tagfile associated with %s" file-base))
+    (setq ido-dynamic-match-fn 'mural-query-save-tag)
+    (unwind-protect ;; i have no idea what's going on here
+        (ido-completing-read "tag: " '("nope"))
+      (setq ido-dynamic-match-fn nil))))
 
+(defun mural-tag-tagname (tag)
+  (elt tag 0))
 (defun mural-tag-filename (tag)
   (elt tag 1))
 (defun mural-tag-row (tag)
@@ -111,18 +179,13 @@
   (let* ((tag (mural-read-tag))
         (taginfo (assoc tag mural-last-query-result)))
     (progn
-      (find-file (mural-abspath-for-tag taginfo))
+      (find-file (mural-abspath-for-tag (file-name-directory mural-current-tagfile) taginfo))
       (goto-line (mural-tag-row taginfo))
       (setq mural-last-query-result nil)
       )))
 
-(defun mural-abspath-for-tag (taginfo)
-  (expand-file-name
-   (mural-tag-filename taginfo)
-   ;; need to make this conditional on the tag file to support multiple repos
-   (file-name-directory (directory-file-name tags-file-name))))
-
-;; (mural-abspath-for-tag '("hi" "flib/core/preparable/Preparer.php" 1))
+(defun mural-abspath-for-tag (root taginfo)
+  (file-truename (expand-file-name (mural-tag-filename taginfo) root)))
 
 (defvar ido-dynamic-last-query nil)
 
